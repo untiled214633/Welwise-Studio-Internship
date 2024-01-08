aba08a9d6d3e594af32f2d86b71fbfe51e36e993
untiled214633 <nameless.unofficial@gmail.com>
2024-01-08 13:34:07 +0300

    simple shooting
    simple shooting realization, i have to take a small break (stop timer)

--- Russian Hospital/Assets/Scripts/PlayerController.cs
+++ Russian Hospital/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Camera _mainCamera;
+    [SerializeField] private GameObject _ball;
+
+
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    public void Shoot(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.phase == InputActionPhase.Performed)
+        {
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            var raycast = Physics.Raycast(ray, out RaycastHit hitInfo);
+
+            _ball.transform.position = hitInfo.point;
+        }
     }
 }
