460c33ee20e4a0ab8ba18ce079dc8c681db51f7f
untiled214633 <nameless.unofficial@gmail.com>
2024-01-08 12:28:46 +0300

    Adding crosshair +removing standart cursor

--- Russian Hospital/Assets/Scripts/PlayerController.cs
+++ Russian Hospital/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,9 @@
-
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
